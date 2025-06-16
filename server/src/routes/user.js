5d03d0eb1b5d649efc94767cae486238ddef2477
Kaylin Khanal <kaylinkhanal@Kaylins-MacBook-Pro-2.local>
2025-06-16 08:52:39 +0545

    added jwt token in login

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const saltRounds = 10
 import User from "../models/user.js"
 const userRouter = Router()
@@ -19,21 +20,25 @@ userRouter.post('/register', async (req, res) => {
 
 
   userRouter.post('/login', async (req, res) => {
+    const {email, password} = req.body
     // ---step 1: email should exist
-    const user = await User.findOne({ email: req.body.email })
+    const user = await User.findOne({ email: email })
     // --- no: return email not found
     if(!user) return res.send({message: 'Email not found'})
    
     // ---yes: 
       // step 2: check if password matches to that of db
-      const isMatched = await bcrypt.compare(req.body.password, user.password)
+      const isMatched = await bcrypt.compare(password, user.password)
       if(!isMatched) return res.send({message: 'Invalid password'})
+      
+      const token = await jwt.sign({ email: email }, '33ceecfc3c914cb6dee77a8c16d08211caf46cfb1aa0b9372f24e39fbaaa1fca31d1a7ae7938579f7ed157636babf812ac42468d431128f7f3098c51a5a4a69a');
+
       return res.send({
           message: 'logged in successfully',
           user: user,
-          isLoggedIn: true
+          isLoggedIn: true,
+          token
         })
-
   })
 
 
