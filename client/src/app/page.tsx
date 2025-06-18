44d4e8d34cac79155e61ef65380ce96b76d9817d
Kaylin Khanal <kaylinkhanal@Kaylins-MacBook-Pro-2.local>
2025-06-18 08:31:54 +0545

    added redux setup

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import { NavigationMenuDemo } from '@/components/navbar'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  const {backgroundColor} = useSelector(state=>state.box)
   return (
     <div>
+{backgroundColor}
       <NavigationMenuDemo/>
       Home</div>
   )
