4454e5117334bd5953a25acd71927f56567438bc
angeltamang123 <tamangangel2057@gmail.com>
2025-07-09 22:25:07 +0545

    chore: handled user preference link

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,33 +1,29 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import counterSlice from './reducerSlices/counterSlice';
-import boxSlice from './reducerSlices/boxSlice';
-import userSlice from './reducerSlices/userSlice';
-import reduxLogger from 'redux-logger';
-import productSlice from './reducerSlices/productSlice'
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import counterSlice from "./reducerSlices/counterSlice";
+import boxSlice from "./reducerSlices/boxSlice";
+import userSlice from "./reducerSlices/userSlice";
+import reduxLogger from "redux-logger";
+import productSlice from "./reducerSlices/productSlice";
 
 const rootReducer = combineReducers({
   counter: counterSlice,
   box: boxSlice,
   user: userSlice,
-  product:productSlice
+  product: productSlice,
 });
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer, 
-  middleware: () =>[reduxLogger]
+  reducer: persistedReducer,
+  middleware: () => [reduxLogger],
 });
 
 export const persistor = persistStore(store);
