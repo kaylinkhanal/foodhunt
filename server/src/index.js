ef8f40470f3d1270ea4df2ca848790ad610b6de5
Laxmi-999 <regmilaxmi325@gmail.com>
2025-07-15 11:55:28 +0545

    added port

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ import categoryRouter from "./routes/category.js";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080 ;
 const app = express();
 
 connect();
